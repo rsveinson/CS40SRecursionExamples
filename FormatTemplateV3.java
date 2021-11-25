4174711e2067c34289037af5829d7aad96d86328
rsveinson <rsveinson@mts.net>
2021-11-25 08:01:41 -0600

    Initial sharing of project

---
+++ FormatTemplateV3.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.io.*;
+import java.text.NumberFormat;
+
+/** ***************************************************
+ *  Name:           Sveinson
+ *  Class:          CS30S
+ * 
+ *  Assignment:     Ax Qy
+ * 
+ *  Description:    Place a short description of your program here
+ * 
+ *************************************************************/
+
+public class FormatTemplateV3 {
+
+    public static void main(String[] args) throws IOException{
+    // ***** constants *******
+    
+    // ***** variables *****
+    
+        String banner = "";             // output banner
+        String prompt = "";             // prompt for user input
+        
+        String strin = "";              // string fro keyboard input
+        String strout = "";             // string for formatted output
+        
+        String delim = "[ ]+";          // delimiter for splitting input records
+        String[] tokens = null;         // used to split input records
+        
+        // a new line character that works on every computer system
+        String nl = System.lineSeparator();
+    
+    // ***** objects *****
+    
+        //Scanner scanner = new Scanner(System.in);
+        //NumberFormat currency = NumberFormat.getCurrencyInstance();
+        
+        // file io buffers for reading and writing to text files
+        
+        //BufferedReader fin = new BufferedReader(new FileReader("filename.txt"));
+        //PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("outfle.txt")));
+    
+    // ***** print banners *****
+    
+        banner = "*****************************" + nl;
+        banner += "Name:        Sveinson" + nl;
+        banner += "Class:       CS30S" + nl;
+        banner += "Assignment:  Ax Qy" + nl;
+        banner += "*****************************" + nl + nl;
+        
+        System.out.println(banner);
+        //fout.print(banner);
+    
+    // ***** Get Input *****
+    
+        // prompt for input
+        // read input from keyboard
+        // echo input back to console window
+    
+    // ***** Main Processing *****
+    
+    // ***** Print Formatted Output *****
+    
+    // ***** Closing Message *****
+    
+        System.out.println();
+        System.out.println("end of processing");
+        //fout.println("End of Processing");
+        
+    // **** close io buffers *****
+    
+        //fin.close();
+        //fout.close();
+    } // end main 
+    
+} // end FormatTemplate
